fba1bb5b71648295260610b74d0798f642f40101
TaaviNat <taavi.natynki@gmail.com>
2023-11-13 10:13:52 +0200

    Module 2 ready

---
+++ Module2/src/EventList.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+public class EventList {
+    private PriorityQueue<Event> eventList;
+
+    public EventList() {
+        eventList = new PriorityQueue<>();
+    }
+
+    public void add(Event e) {
+        System.out.println("Adding to the event list " + e.getType() + " " + e.getTime());
+        eventList.add(e);
+    }
+
+    public Event remove() {
+        System.out.println("Removing from the event list " + eventList.peek().getType() + " " +  eventList.peek().getTime());
+        return eventList.remove();
+    }
+
+    public void print() {
+        Object[] tmp = eventList.toArray();
+        Arrays.sort(tmp);
+        for (Object e : tmp) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        EventList eventList = new EventList();
+        Random random = new Random();
+        // generate events
+        for (int i = 0; i < 10; i++) {
+            EvenType type;
+            if (random.nextBoolean())
+                type = EvenType.ARR;
+            else
+                type = EvenType.DEP;
+            eventList.add(new Event(type, random.nextInt(1000)));
+        }
+
+        // remove the first event to be processed
+        System.out.println("Event removed " + eventList.remove());
+
+        // finally print the whole event list
+        eventList.print();
+
+    }
+}
